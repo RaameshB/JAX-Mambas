2528a7c3e1d3534829b45311edabb5aafe9f4607
raameshb <raameshb@proton.me>
2026-07-28 12:14:45 -0500

    refactor the test to run non kernel version first and add sanity

--- test_mamba1.py
+++ test_mamba1.py
@@ -4,22 +4,21 @@ if __name__ == "__main__":
     from jax.numpy import isclose
     from icecream import ic
 
-    rngs = Rngs(1)
-    test_inp = rngs.uniform((3, 32, 2))
-
     D = 4
     N = 8
     K = 8
-
-    s6 = S6(rngs, D=D, N=N, use_kernel=True, kernel_seq_len=K)
-    kernel_out = s6(test_inp)
-    ic(kernel_out)
 
     rngs = Rngs(1)
     test_inp = rngs.uniform((3, 32, 2))
     s6 = S6(rngs, D=D, N=N, use_kernel=False)
     reference_out = s6(test_inp)
     ic(reference_out)
+
+    rngs = Rngs(1)
+    test_inp = rngs.uniform((3, 32, 2))
+    s6 = S6(rngs, D=D, N=N, use_kernel=True, kernel_seq_len=K)
+    kernel_out = s6(test_inp)
+    ic(kernel_out)
 
     assert isclose(kernel_out, reference_out).all()
 
