2ef1f36a35ff412b4a8e5e4153a572b1badc1e6d
raameshb <raameshb@proton.me>
2026-07-28 13:57:07 -0500

    fix old kernel implementation

--- mamba1.py
+++ mamba1.py
@@ -362,13 +362,12 @@ class S6(nnx.Module):
         if not self.use_kernel:
             A_bars, B_bars = self.discretize(A, Bs, Deltas)
             Bx = B_bars * x[..., jnp.newaxis]
-            xs = lax.associative_scan(S6.binary_operator, (A_bars, Bx), axis=1)
-            ic(xs.shape)
+            _, xs = lax.associative_scan(S6.binary_operator, (A_bars, Bx), axis=1)
             self.state_caches.value = xs[:, -1:, ...]
             ys = jnp.einsum("bln,bldn->bld", Cs, xs)
         else:
             # xs = self.apply_with_kernel(A, Bs, Deltas, x)
-            _, xs = self.apply_with_kernel(A, Bs, Deltas, x)
+            xs = self.apply_with_kernel(A, Bs, Deltas, x)
             self.state_caches.value = xs[:, -1:, ...]
             ys = jnp.einsum("bln,bldn->bld", Cs, xs)
         ic(xs.shape)
