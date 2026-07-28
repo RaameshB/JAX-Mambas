e87f2a8597c4323bc4f791ceae48f0153dff2d71
raameshb <raameshb@proton.me>
2026-07-28 12:16:39 -0500

    fix embedding dim in test

--- test_mamba1.py
+++ test_mamba1.py
@@ -9,13 +9,13 @@ if __name__ == "__main__":
     K = 8
 
     rngs = Rngs(1)
-    test_inp = rngs.uniform((3, 32, 2))
+    test_inp = rngs.uniform((3, 32, D))
     s6 = S6(rngs, D=D, N=N, use_kernel=False)
     reference_out = s6(test_inp)
     ic(reference_out)
 
     rngs = Rngs(1)
-    test_inp = rngs.uniform((3, 32, 2))
+    test_inp = rngs.uniform((3, 32, D))
     s6 = S6(rngs, D=D, N=N, use_kernel=True, kernel_seq_len=K)
     kernel_out = s6(test_inp)
     ic(kernel_out)
