e448e14e31122c92a74c7061fbc952798964a219
raameshb <raameshb@proton.me>
2026-08-05 23:40:36 -0500

    remove the run script for selective copying too

--- experiments/run_selectivecopying.sh
+++
@@ -1,4 +0,0 @@
-pip uninstall -y jax-cuda12-plugin jax-cuda12-pjrt jaxlib
-pip install -U flax optax jax[cuda13]
-git clone https://github.com/RaameshB/JAX-Mambas.git
-cd JAX-Mambas && git switch dev && git pull && python experiments/mamba_selectivecopying.py
